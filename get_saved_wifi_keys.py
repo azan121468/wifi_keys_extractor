5d67dc420115b4be2b900719aa8e0488c3f81743
Azan Shahid <67824275+azan121468@users.noreply.github.com>
2020-08-19 12:01:25 -0700

    Update get_saved_wifi_keys.py

--- get_saved_wifi_keys.py
+++ get_saved_wifi_keys.py
@@ -1,28 +1,11 @@
 import subprocess
-results = subprocess.check_output(["netsh", "wlan", "show", "network"])
-results = results.decode("ascii") # needed in python 3
-results = results.replace("\r","")
-ls = results.split("\n")
-ls = ls[4:]
-ssids = []
-x = 0
-while x < len(ls):
-    if x % 5 == 0:
-        ssids.append(ls[x])
-    x += 1
-j=0
-for i in ssids:
-    ssids[j]=i[9:]
-    j+=1
-for x in ssids:
-    if x=='':
-        ssids.remove(x)
-    else:
-        x.strip()
-for i in ssids:
+
+data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+for i in profiles:
     results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
     results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
     try:
-        print(f"{i} : {results[0]}")
+        print ("{:<20} : {:<}".format(i, results[0]))
     except IndexError:
-        print(f"{i} : {''}")
+        print ("{:<20} : {:<}".format(i, ""))
